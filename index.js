9e016ef7758da5532a834b1d4581861e686ac37c
Ryan Mottola <rmottola68@gmail.com>
2022-06-06 13:58:12 -0400

    commit tree 52b5f52cfc847b51452d862c8b192eb62b6d7453

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 5;
+const num4 = 4;
+const num5 = 20;
 
-// Write your code here
+let multiply = num1*num2;
+
+let random = Math.ceil(Math.random(num2, num1)+1);
+
+let mod = num4 % num3
+
+let max = Math.max(4, 7, 20, 18);
+
+console.log(num2, num3, num4, num5);
